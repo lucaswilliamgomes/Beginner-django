bb0681f3cc3d78bde47a11319a2ef0c99cd2ec34
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-02-09 11:04:43 -0300

    hello world

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homepage/', views.home, name='home')
 ]
