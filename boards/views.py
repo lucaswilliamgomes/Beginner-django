e4d30eb468a7bf17b379df139b28b2ce2ae7f9b4
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-02-11 18:09:12 -0300

    Add new topic feature

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from boards.models import Board
+from django.shortcuts import render, get_object_or_404, redirect
+from boards.models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -10,3 +11,31 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render (request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+    
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        
+        return redirect(board_topics, pk=board.pk) 
+
+    return render(request, 'new_topic.html', {'board': board})
+
+    
