5609faac4af9e2ac89a82f23027267bbb2f74d55
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-02-11 10:35:41 -0300

    Create topics page, navigation with home and tests

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from boards.models import Board
 
 
@@ -6,3 +6,7 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
     
+    
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render (request, 'topics.html', {'board': board})
