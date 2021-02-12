fd3d6275dd30fc2535a9114fec3a4f280631f058
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-02-12 01:11:20 -0300

    validate form fields new topic and tests

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from boards.models import Board, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 
 def home(request):
@@ -15,27 +16,20 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first()
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-    
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-        
-        return redirect(board_topics, pk=board.pk) 
-
-    return render(request, 'new_topic.html', {'board': board})
-
-    
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
