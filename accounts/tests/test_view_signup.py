75fa4c08ada9c44c704a3c458e68ebe8b1b1a75d
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-02-15 19:09:29 -0300

    create page login and tests

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,21 +1,35 @@
 from django.urls import resolve, reverse
 from django.test import TestCase
-from .views import signup
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .views import signup
- 
+from ..forms import SignUpForm
+from ..views import signup
 
 class SignUpTests(TestCase):
-    
-    def test_signup_status_code(self):
+
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+    
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SignUpForm)
+    
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class SucessfulSignUpTests(TestCase):
@@ -24,6 +38,7 @@ class SucessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
